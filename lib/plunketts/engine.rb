e7f58625df0b848df49128cd9a11de75cd0d6a53
Andy Selvig <andy@Flame.local>
2019-01-09 18:52:14 -0600

    don't require ScheduleRule in the engine, I guess

--- lib/plunketts/engine.rb
+++ lib/plunketts/engine.rb
@@ -14,8 +14,6 @@ require 'plunketts/io/cvs_io'
 require 'plunketts/logging/loggable'
 require 'plunketts/logging/multi_logger'
 
-require 'plunketts/schedule_rule'
-
 module Plunketts
   class Engine < ::Rails::Engine
   end
