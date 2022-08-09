beaefe75a9cc43f03660b204428802b16ea2e33d
Mark Wickline <mwicklinedev@gmail.com>
2022-08-09 09:25:34 -0500

    move email inside persistent check

--- lib/terrier/scripts/script_executor.rb
+++ lib/terrier/scripts/script_executor.rb
@@ -52,9 +52,9 @@ class ScriptExecutor
       script_run.duration = Time.now - t
       if @script.persisted? # we can't save the run if it's a temporary script
         script_run.write_log @log_lines.join("\n")
+        script_email_log = @script.send_email_if_necessary @output_files, script_run.log_url, me.full_name
+        puts script_email_log unless script_email_log.empty?
       end
-      script_email_log = @script.send_email_if_necessary @output_files, script_run.log_url, me.full_name
-      puts script_email_log unless script_email_log.empty?
       true
     rescue => ex
       line = ex.backtrace[0].split(':')[1].to_i
