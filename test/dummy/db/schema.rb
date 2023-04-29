6f505113d4b20155957a8dac143f01db7068d6e0
Andy Selvig <ajselvig@gmail.com>
2023-04-29 09:54:00 -0500

    vite integration in dummy

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -2,23 +2,22 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_13_185004) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_185004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "locations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "_state", default: 0, null: false
     t.uuid "created_by_id"
     t.text "created_by_name", null: false
@@ -43,8 +42,8 @@ ActiveRecord::Schema.define(version: 2023_04_13_185004) do
   end
 
   create_table "script_runs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "_state", default: 0, null: false
     t.uuid "created_by_id"
     t.text "created_by_name", null: false
@@ -58,7 +57,7 @@ ActiveRecord::Schema.define(version: 2023_04_13_185004) do
     t.string "log_file_name"
     t.string "log_content_type"
     t.bigint "log_file_size"
-    t.datetime "log_updated_at"
+    t.datetime "log_updated_at", precision: nil
     t.string "status", default: "success", null: false
     t.uuid "script_id", null: false
     t.text "org_id"
@@ -71,8 +70,8 @@ ActiveRecord::Schema.define(version: 2023_04_13_185004) do
   end
 
   create_table "scripts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "_state", default: 0, null: false
     t.uuid "created_by_id"
     t.text "created_by_name", null: false
@@ -100,8 +99,8 @@ ActiveRecord::Schema.define(version: 2023_04_13_185004) do
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "_state", default: 0, null: false
     t.uuid "created_by_id"
     t.text "created_by_name", null: false
@@ -120,7 +119,7 @@ ActiveRecord::Schema.define(version: 2023_04_13_185004) do
     t.text "notes_raw"
     t.text "password_digest", null: false
     t.text "password_reset_token"
-    t.datetime "password_reset_token_expires_at"
+    t.datetime "password_reset_token_expires_at", precision: nil
     t.text "role", default: "tech", null: false
     t.text "state"
     t.text "tags", default: [], null: false, array: true
---
+++ test/dummy/public/vite-dev/assets/app-0afbd816.js
@@ -0,0 +1,1 @@
+console.log("Vite ⚡️ Rails in Terrier-Engine");console.log("Visit the guide for more information: ","https://vite-ruby.netlify.app/guide/rails");
