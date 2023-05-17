b9344de41596aa838f255cd508359db305b4acf2
Andy Selvig <ajselvig@gmail.com>
2023-05-17 10:02:15 -0500

    section click styling

--- app/frontend/data-dive/queries/tables.ts
+++ app/frontend/data-dive/queries/tables.ts
@@ -4,6 +4,10 @@ import {SchemaDef} from "../../terrier/schema"
 import inflection from "inflection"
 import Filters, {Filter} from "./filters"
 import Columns, {ColumnRef} from "./columns"
+import {messages} from "tuff-core"
+import {Logger} from "tuff-core/logging"
+
+const log = new Logger("Tables")
 
 ////////////////////////////////////////////////////////////////////////////////
 // Types
@@ -36,10 +40,16 @@ export class TableEditor<T extends TableRef> extends DdContentPart<{ schema: Sch
     tableName!: string
     joinedEditors: JoinedTableEditor[] = []
 
+    editColumnsKey = messages.untypedKey()
+
     async init() {
         this.schema = this.state.schema
         this.table = this.state.table
         this.makeJoinedEditors()
+
+        this.onClick(this.editColumnsKey, _ => {
+            log.info(`Edit ${this.tableName} Columns`)
+        })
     }
 
     /**
@@ -84,6 +94,7 @@ export class TableEditor<T extends TableRef> extends DdContentPart<{ schema: Sch
             })
             this.renderColumns(panel)
             this.renderFilters(panel)
+            this.renderJoins(panel)
         })
     }
 
@@ -101,9 +112,9 @@ export class TableEditor<T extends TableRef> extends DdContentPart<{ schema: Sch
                 }
             }
             else {
-                section.div('.line').div('.empty').text('None')
+                section.div('.line.empty').div().text('None')
             }
-        })
+        }).emitClick(this.editColumnsKey, {})
     }
 
     renderFilters(parent: PartTag) {
@@ -119,8 +130,21 @@ export class TableEditor<T extends TableRef> extends DdContentPart<{ schema: Sch
                     })
                 }
             } else {
-                section.div('.line').div('.empty').text('None')
+                section.div('.line.empty').text('None')
             }
+        })
+    }
+
+    renderJoins(parent: PartTag) {
+        parent.section(section => {
+            section.div('.title', title => {
+                title.i(".glyp-belongs_to")
+                title.span({text: "Joins"})
+            })
+            section.div('.line.empty', line => {
+                line.i('.glyp-plus')
+                line.div().text('Belongs-To')
+            })
         })
     }
 
