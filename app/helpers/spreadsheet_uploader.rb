9706d682a596fac164b124586e989bc1564ee1d2
Andy Selvig <ajselvig@gmail.com>
2023-07-14 11:08:40 -0500

    increase upload size limit for spreadsheets

--- app/helpers/spreadsheet_uploader.rb
+++ app/helpers/spreadsheet_uploader.rb
@@ -2,7 +2,7 @@ class SpreadsheetUploader < Shrine
   plugin :determine_mime_type, analyzer: :marcel
 
   Attacher.validate do
-    validate_size 1..10 * 1024 * 1024 # MB
+    validate_size 1..100 * 1024 * 1024 # MB
     validate_mime_type %w[text/csv application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/zip]
   end
 end
