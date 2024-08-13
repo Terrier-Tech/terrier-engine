f6a75c71ddd316f2b6fc92488e2af224230ef617
Andy Selvig <ajselvig@gmail.com>
2024-08-13 14:07:07 -0500

    add the ability to clamp dive plot axes to zero

--- app/frontend/data-dive/plots/dive-plot-axes.ts
+++ app/frontend/data-dive/plots/dive-plot-axes.ts
@@ -4,34 +4,49 @@ import {PartTag} from "tuff-core/parts"
 import TerrierPart from "../../terrier/parts/terrier-part"
 import {DbErrors} from "../../terrier/db-client"
 import * as inflection from "inflection"
-import DivePlotEditor from "./dive-plot-editor";
+import DivePlotEditor from "./dive-plot-editor"
 
 
 // let's not worry about a top axis for now
 const axisSides = ['left', 'bottom', 'right'] as const
 export type AxisSide = typeof axisSides[number]
 
+// we have some additional axis types that affect the tick format
 export type DivePlotAxisType = AxisType | 'dollars' | 'days' | 'months' | 'none'
 
 export type DivePlotAxis = {
     type: DivePlotAxisType
     title: string
+    clampToZero?: boolean
 }
 
-const axisTypeOptions = [
+const leftAxisTypeOptions = [
     {value: 'number', title: 'Number'},
     {value: 'dollars', title: 'Dollars'},
     {value: 'days', title: 'Dates'},
-    {value: 'months', title: 'Months'},
-    {value: 'group', title: 'Grouped Bars'},
-    {value: 'stack', title: 'Stacked Bars'},
+    {value: 'months', title: 'Months'}
 ]
 
-// the right axis isn't required
-const rightAxisTypeOptions = axisTypeOptions.concat([
-    {value: 'none', title: 'None'}
+// only allow vertical bars
+const bottomAxisTypeOptions = leftAxisTypeOptions.concat([
+    {value: 'group', title: 'Grouped Bars'},
+    {value: 'stack', title: 'Stacked Bars'}
 ])
 
+// the right axis isn't required
+const rightAxisTypeOptions = [
+    {value: 'none', title: 'None'}
+].concat(leftAxisTypeOptions)
+
+const axisTypeOptions = {
+    left: leftAxisTypeOptions,
+    right: rightAxisTypeOptions,
+    bottom: bottomAxisTypeOptions
+} as const
+
+/**
+ * Fields for a DivePlotAxis.
+ */
 export class DivePlotAxisFields extends TerrierFormFields<DivePlotAxis> {
 
     constructor(part: TerrierPart<any>, axis: DivePlotAxis, readonly side: AxisSide, errors?: DbErrors<DivePlotAxis>) {
@@ -47,12 +62,22 @@ export class DivePlotAxisFields extends TerrierFormFields<DivePlotAxis> {
                 .emitChange(DivePlotEditor.relayoutKey)
 
             // type
-            const typeOptions = this.side == 'right' ? rightAxisTypeOptions : axisTypeOptions
+            const typeOptions = axisTypeOptions[this.side]
             this.select(container, 'type', typeOptions)
                 .emitChange(DivePlotEditor.relayoutKey)
+
+            if (this.side == 'left' || this.side == 'right') {
+                container.label('.caption-size', label => {
+                    this.checkbox(label, 'clampToZero')
+                        .emitChange(DivePlotEditor.relayoutKey)
+                    label.div().text("Clamp to Zero")
+                }).data({tooltip: "Ensure that the axis range includes zero"})
+
+            }
         })
     }
 }
+
 
 /**
  * Convert a DivePlotAxis to a tuff-plot PlotAxis.
@@ -64,9 +89,9 @@ function toPlotAxis(diveAxis: DivePlotAxis): PlotAxis | undefined {
         return undefined
     }
     let tickMode: PlotAxis['tickMode'] = 'auto'
-    let type: AxisType = 'number'
+    let type: AxisType
     let tickFormat: PlotAxis['tickFormat'] = '0.[0]a'
-    let hoverFormat = '0.[0]a'
+    let hoverFormat = tickFormat
     switch (diveAxis.type) {
         case 'dollars':
             type = 'number'
@@ -93,7 +118,7 @@ function toPlotAxis(diveAxis: DivePlotAxis): PlotAxis | undefined {
         tickFormat,
         hoverFormat,
         title: diveAxis.title,
-        range: "auto",
+        range: diveAxis.clampToZero ? 'auto_zero' : 'auto',
         tickLength: 6
     }
 }
