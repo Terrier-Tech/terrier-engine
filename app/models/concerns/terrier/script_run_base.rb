85117d9130e62993a368f31777dc3f187ab2aeac
Michael Werdal <michael@terrier.tech>
2024-10-12 14:32:03 -0500

    move filtered_fields to script_run_base

--- app/models/concerns/terrier/script_run_base.rb
+++ app/models/concerns/terrier/script_run_base.rb
@@ -10,6 +10,16 @@ module Terrier::ScriptRunBase
 
     validates :duration, presence: true
 
+    def filtered_fields
+      SqlBuilder.new
+                .with("jsonb_data as (select fields from script_runs where id = '#{self.id}')")
+                .select('jsonb_object_agg(key, value) as fields')
+                .from('jsonb_data, jsonb_each(jsonb_data.fields::jsonb)')
+                .where("jsonb_typeof(value) != 'array'")
+                .as_objects
+                .exec.to_a.first.fields
+    end
+
   end
 
 end
