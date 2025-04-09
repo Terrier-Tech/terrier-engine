e04034744bef02d3d45115ee2295ffe73e1db4ed
Cole Schultz <cole@terrier.tech>
2025-04-09 08:29:44 -0500

    fix multilogger error when logging exception with nested loggers
    when there are nested multiloggers, the first multilogger will convert the exception to a string, and then subsequent loggers will try to call `message`, which fails. This allows
    
    also add doc comments/type annotations

--- lib/terrier/logging/multi_logger.rb
+++ lib/terrier/logging/multi_logger.rb
@@ -7,13 +7,27 @@ class MultiLogger
 
   attr_accessor :use_stdout, :use_rails, :logger, :stream, :level, :prefix, :messages
 
-  LEVELS = %w(debug info success separator warn error)
+  LEVELS = %w(debug info success separator warn error).freeze
 
-  def initialize(prefix, opts={})
+  LEVEL_VALUES = {
+    'debug' => 0,
+    'info' => 1,
+    'success' => 2,
+    'separator' => 3,
+    'warn' => 4,
+    'error' => 5,
+  }.freeze
+
+  # @param prefix [String] a prefix added to each log message
+  # @param opts [Hash] deprecated. Use specific keyword arguments
+  # @param logger [::Logger, ActiveSupport::Logger, MultiLogger, nil] an inner logger to log to
+  # @param use_stdout [Boolean, nil] whether to log using `puts`. If nil, defaults to false if an inner logger is provided, otherwise true
+  # @param use_rails [Boolean, nil] whether to log using Rails.logger. If nil, defaults to false if an inner logger is provided, otherwise true
+  def initialize(prefix, opts={}, logger: nil, use_stdout: nil, use_rails: nil)
     @prefix = prefix
-    @logger = opts[:logger]
-    @use_stdout = opts[:use_stdout]
-    @use_rails = opts[:use_rails]
+    @logger = logger || opts[:logger]
+    @use_stdout = use_stdout || opts[:use_stdout]
+    @use_rails = use_rails || opts[:use_rails]
 
     # if a specific logger is passed in, use_stdout and use_rails are opt-in (off by default)
     # if a specific logger is not passed in, use_stdout and use_rails are opt-out (on by default)
@@ -24,58 +38,81 @@ class MultiLogger
     @messages = []
   end
 
+  # Opens a response stream to log to
+  # @param response [ActionDispatch::Response] the response to stream to
   def stream_response(response)
     response.headers["Content-Type"] = "layout/json"
     @stream = response.stream
     @stream.write '['
   end
 
+  # Closes the previously opened response stream
   def close_stream
     return unless @stream
     @stream.write '{}]'
     @stream.close
   end
 
-  # Setter method to change the log level
-  def level=(new_level)
-    @level = new_level
-  end
-
+  # Logs the given message at debug level
+  # @param message [String]
+  # @param args [Array] an array of objects that will be AwesomePrinted after the message
   def debug(message, *args)
     log 'debug', message, *args
   end
 
+  # Logs the given message at info level
+  # @param message [String]
+  # @param args [Array] an array of objects that will be AwesomePrinted after the message
   def info(message, *args)
     log 'info', message, *args
   end
 
+  # Logs the given message at success level
+  # For loggers that don't support success as a level, uses 'info'
+  # @param message [String]
+  # @param args [Array] an array of objects that will be AwesomePrinted after the message
   def success(message, *args)
     log 'success', message, *args
   end
 
+  # Logs the given message at warn level
+  # @param message [String]
+  # @param args [Array] an array of objects that will be AwesomePrinted after the message
   def warn(message, *args)
     log 'warn', message, *args
   end
 
+  # Logs the given message as a "separator", i.e. the message is surrounded with equals signs
+  # @param message [String]
+  # @param args [Array] an array of objects that will be AwesomePrinted after the message
   def separator(message, *args)
+    message = "==== #{message} ===="
     log 'separator', message, *args
   end
 
+  # Logs the given exception or message at error level
+  # @param ex [Exception, String]
+  # @param args [Array] an array of objects that will be AwesomePrinted after the message
   def error(ex, *args)
-    message = ex.message
-    unless ex.backtrace.nil?
-      ex.backtrace[0..20].each do |line|
-        message += "\n#{line}"
+    if ex.is_a?(Exception)
+      message = ex.message
+      unless ex.backtrace.nil?
+        ex.backtrace[0..20].each do |line|
+          message += "\n#{line}"
+        end
       end
+    else
+      message = ex
     end
     log 'error', message, *args
   end
 
+  # Logs the given message at the given log level
+  # @param level [String] one of 'debug', 'info', 'success', 'separator', 'warn', 'error'
+  # @param message [String]
+  # @param args [Array] an array of objects that will be AwesomePrinted after the message
   def log(level, message, *args)
-    return if LEVELS.index(@level) > LEVELS.index(level)
-    if level == 'separator'
-      message = "==== #{message} ===="
-    end
+    return if LEVEL_VALUES[@level] > LEVEL_VALUES[level]
     write level, message
 
     # amazing_print extra args
@@ -102,11 +139,17 @@ class MultiLogger
 
 
   # writes directly to the appropriate outputs
+  # @param level [String] one of 'debug', 'info', 'success', 'separator', 'warn', 'error'
+  # @param s [Object] the message to log
   def write(level, s)
     logger_level = %w(separator success).index(level) ? 'info' : level
     logger_message = "#{@prefix} #{s}"
     if @logger
-      @logger.send logger_level, logger_message
+      if @logger.respond_to?(level)
+        @logger.send level, logger_message
+      else
+        @logger.send logger_level, logger_message
+      end
     end
 
     if Rails&.logger && @use_rails
@@ -120,6 +163,9 @@ class MultiLogger
     write_stream level, stdout_message
   end
 
+  # writes output as a json object to @messages and the output stream, if opened
+  # @param level [String] one of 'debug', 'info', 'success', 'separator', 'warn', 'error'
+  # @param s [Object] the message to log
   def write_stream(level, s)
     time = Time.now.strftime(PRETTY_TIME_FORMAT)
     @messages << {prefix: @prefix, time: time, level: level, message: s}
