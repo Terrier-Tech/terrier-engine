6b096a75bc1ba10ef0871b2380a1888eb4df927f
Calvin Theis <calvin.theis@terrier.tech>
2026-03-12 14:19:53 -0500

    fix(parts): fix null safety in `getNamedAction`

--- app/frontend/terrier/parts/content-part.ts
+++ app/frontend/terrier/parts/content-part.ts
@@ -94,7 +94,7 @@ export default abstract class ContentPart<TState> extends TerrierPart<TState> {
      * @param name
      */
     getNamedAction(name: string): Action | undefined {
-        return this._namedActions[name].action
+        return this._namedActions[name]?.action
     }
 
     /**
